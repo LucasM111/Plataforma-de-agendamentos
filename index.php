bf0971e27bf74f50bfe449eab85df3bdb8176886
Lucas Marques <94138717+LucasM111@users.noreply.github.com>
2023-05-22 13:49:52 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!----- arquivos css ---->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/datatables.min.css">
+    <link rel="stylesheet" href="css/summernote-lite.min.css">
+    <link rel="stylesheet" href="css/sweetalert2.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <!----- arquivos javascript ---->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/datatables.min.js"></script>
+    <script src="js/parsley.min.js"></script>
+    <script src="js/summernote-lite.min.js"></script>
+    <script src="js/summernote-pt-BR.js"></script>
+    <script src="js/sweetalert2.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/vanilla-masker.js"></script>
+
+
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="imagens/Ltech.png" alt="Ltech" width="80" height="80" class = "ltech">
+    </a>
+  </div>
+</nav>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Agendamentos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Sobre a Plataforma</a>
+        </li>
+
+      </ul>
+      
+    </div>
+</ul>
+  </div>
+</nav>
+
+
+
+</body>
+</html>
